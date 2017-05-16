365cd64daadaf23566c2d017df2aa7b4178cda32
Nitish Puri <npuri1903@gmail.com>
2017-05-16 21:37:45 +0530

    project solution.

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -20,6 +20,13 @@ from email_preprocess import preprocess
 features_train, features_test, labels_train, labels_test = preprocess()
 
 
+outputFile = "rbf5.txt"
+out = open(outputFile, "w")
+
+def logLine(line):
+    out.write(line)
+    out.write("\n")
+    print line
 
 
 #########################################################
@@ -27,4 +34,30 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+
+C = 10000.0
+clf = SVC(kernel='rbf', C=C)
+# features_train = features_train[:len(features_train)/100]
+# labels_train = labels_train[:len(labels_train)/100]
+t0 = time()
+clf.fit(features_train, labels_train)
+t1 = time()
+labels_predict = clf.predict(features_test)
+t2 = time()
+accuracy = accuracy_score(labels_test, labels_predict)
+
+# logLine("Training on 1% data with rbf kernel. C = {}".format(C))
+logLine("Training on full data with rbf kernel. C = {}".format(C))
+logLine("Accuracy : {}".format(accuracy))
+logLine("Training time : {}".format(t1-t0))
+logLine("Prediction time : {}".format(t2-t1))
+logLine("Predicion for 10 : {}".format(labels_predict[10]))
+logLine("Predicion for 26 : {}".format(labels_predict[26]))
+logLine("Predicion for 50 : {}".format(labels_predict[50]))
+logLine("Chris emails {}".format(len([x for x in labels_predict if x == 1])))
+
+
+out.close()
 

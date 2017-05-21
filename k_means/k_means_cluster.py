318177495af32be52f0e528db2cb1dd8013c1ac2
Nitish Puri <npuri1903@gmail.com>
2017-05-21 16:52:42 +0530

    K means clustering with 3 features without feature scaling.

--- k_means/k_means_cluster.py
+++ k_means/k_means_cluster.py
@@ -9,11 +9,20 @@
 
 import pickle
 import numpy
+from time import time
 import matplotlib.pyplot as plt
 import sys
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 
+
+outputFile = "output.txt"
+out = open(outputFile, "w")
+
+def logLine(line):
+    out.write(line)
+    out.write("\n")
+    print line
 
 
 
@@ -43,34 +52,77 @@ data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r")
 ### there's an outlier--remove it! 
 data_dict.pop("TOTAL", 0)
 
+# print data_dict["salary"]
+
+
+
 
 ### the input features we want to use 
 ### can be any key in the person-level dictionary (salary, director_fees, etc.) 
 feature_1 = "salary"
 feature_2 = "exercised_stock_options"
+feature_3 = "total_payments"
 poi  = "poi"
-features_list = [poi, feature_1, feature_2]
+
+# def logMinMax(feature):
+#     fmin = numpy.min(data_dict[feature])
+#     fmax = numpy.max(data_dict[feature])
+#     logLine("{} :: Min : {}   Max : {}".format(feature, fmin, fmax))
+
+# logMinMax(feature_1)
+# logMinMax(feature_2)
+# logMinMax(feature_3)
+
+features_list = [poi, feature_1, feature_2, feature_3]
+# features_list = [poi, feature_1, feature_2]
 data = featureFormat(data_dict, features_list )
 poi, finance_features = targetFeatureSplit( data )
 
+def logMinMax(feature, featureList):
+    # featureList = featureList[numpy.logical_not(numpy.equal(featureList, 0))]
+    fmin = numpy.min(featureList)
+    fmax = numpy.max(featureList)
+    logLine("{} :: Min : {}   Max : {}".format(feature, fmin, fmax))
+
+logMinMax(feature_1, finance_features[0])
+logMinMax(feature_2, finance_features[1])
+logMinMax(feature_3, finance_features[2])
 
 ### in the "clustering with 3 features" part of the mini-project,
 ### you'll want to change this line to 
 ### for f1, f2, _ in finance_features:
 ### (as it's currently written, the line below assumes 2 features)
-for f1, f2 in finance_features:
+for f1, f2, f3 in finance_features:
+# for f1, f2 in finance_features:
     plt.scatter( f1, f2 )
 plt.show()
 
 ### cluster here; create predictions of the cluster labels
 ### for the data and store them to a list called pred
 
+from sklearn.cluster import KMeans
+from sklearn.metrics import accuracy_score
+clf = KMeans(n_clusters=2)
+t0 = time()
+clf.fit(finance_features, poi)
+t1 = time()
+labels_predict = clf.predict(finance_features)
+t2 = time()
+accuracy = accuracy_score(poi, labels_predict)
 
+logLine("Accuracy : {}".format(accuracy))
+logLine("Training time : {}".format(t1-t0))
+logLine("Prediction time : {}".format(t2-t1))
+
+pred = labels_predict
+
+
+out.close()
 
 
 ### rename the "name" parameter when you change the number of features
 ### so that the figure gets saved to a different file
 try:
-    Draw(pred, finance_features, poi, mark_poi=False, name="clusters.pdf", f1_name=feature_1, f2_name=feature_2)
+    Draw(pred, finance_features, poi, mark_poi=False, name="clusters3.png", f1_name=feature_1, f2_name=feature_2)
 except NameError:
     print "no predictions object named pred found, no clusters to plot"
